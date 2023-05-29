5da7df17a978b746b73be5796d4a8455f6f476d5
leonardolucas123 <ll9607609@gmail.com>
2023-05-29 14:39:23 -0300

    a

--- rodape.php
+++ rodape.php
@@ -1,8 +1,8 @@
 </div>
 <script type="text/javascript"
-src="./resources/lib/jquery/js/jquery-3.1.1.js"></script>
+src="./js/jquery-3.7.0.js"></script>
 <script type="text/javascript"
-src="./resources/lib/bootstrap/js/bootstrap.js"></script>
-<script type="text/javascript" src="./resources/common/js/crud.js"></script>
+src="./js/bootstrap.js"></script>
+<script type="text/javascript" src="./js/crud.js"></script>
 </body>
 </html>
