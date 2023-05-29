fb8a608112b24d64eecda584832425f89121704d
leonardolucas123 <ll9607609@gmail.com>
2023-05-29 14:55:27 -0300

    a

--- conteudo.php
+++ conteudo.php
@@ -6,7 +6,7 @@ role="dialog">
 <div class="modal-dialog" role="document">
 <div class="modal-content">
 <div class="modal-header">
-<button type="button" class="close" data-dismiss="modal"
+<button type="button" class="btn-close" data-dismiss="modal"
 aria-label="Fechar">
 <span aria-hidden="true">&times;</span>
 </button>
