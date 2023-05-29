5bedd5658dfa1c26e8824a98dfdcbdb514cc175b
GabrielSantanaL <116109002+GabrielSantanaL@users.noreply.github.com>
2023-05-29 14:41:03 -0300

    Update conteudo.php

--- conteudo.php
+++ conteudo.php
@@ -1,10 +1,10 @@
 <!-- exibição de conteúdo -->
-<div id="conteudo"></div>
+<div id="conteudo"></div>
 <!-- caixa de exclusão -->
 <div id="excluir_contato_modal" class="modal fade" tabindex="-1"
-role="dialog">
+role="dialog">
 <div class="modal-dialog" role="document">
-<div class="modal-content">
+<div class="modal-content">
 <div class="modal-header">
 <button type="button" class="close" data-dismiss="modal"
 aria-label="Fechar">
@@ -13,12 +13,12 @@ aria-label="Fechar">
 <h4 class="modal-title">Excluir Contato</h4>
 </div>
 <div class="modal-body">
-<p>Confirma a exclus&atilde;o do contato?</p>
+<p>Confirma a exclusão do contato?</p>
 </div>
-<div class="modal-footer">
+<div class="modal-footer">
 <input type="hidden" id="contato_id" value="" />
 <button type="button" id="excluir" class="btn btn-primary">Sim</button>
-<button type="button" class="btn btn-default" data-dismiss="modal">N&atilde;o</button>
+<button type="button" class="btn btn-default" data-dismiss="modal">Não</button>
 </div>
 </div>
 </div>
