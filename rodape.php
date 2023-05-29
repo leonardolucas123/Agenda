09a43714db752d08c124b8ac240c7e1f6288ec83
GabrielSantanaL <116109002+GabrielSantanaL@users.noreply.github.com>
2023-05-29 14:47:57 -0300

    Update rodape.php

--- rodape.php
+++ rodape.php
@@ -1,10 +1,6 @@
 </div>
 <script type="text/javascript"
-<<<<<<< HEAD
-src="./resources/lib/jquery/js/jquery-3.1.1.js"></script>
-=======
-src="./js/jquery-3.7.0.js"></script>
->>>>>>> 96b48115d06b6d55346203449993ab564d91064e
+src="./js/jquery-3.7.0.js"></script>
 <script type="text/javascript"
 src="./js/bootstrap.js"></script>
 <script type="text/javascript" src="./js/crud.js"></script>
