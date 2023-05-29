f61305d35569fcc241882841753633127b5533aa
leonardolucas123 <ll9607609@gmail.com>
2023-05-29 15:09:44 -0300

    a

--- nav.php
+++ nav.php
@@ -1,13 +1,11 @@
 <nav class="navbar navbar-default">
-<div class="container">
-<ul class="nav navbar-nav">
-<li><a href="javascript:void(0);" id="listar_contato"><i
-class="glyphicon glyphicon-th-list"></i> Listar</a></li>
-<li><a href="javascript:void(0);" id="novo_contato"><i
-class="glyphicon glyphicon-plus"></i> Novo</a></li>
-</ul>
-</div>
+  <div class="container">
+    <ul class="nav navbar-nav">
+      <li><a href="javascript:void(0);" id="listar_contato"><i class="glyphicon glyphicon-th-list"></i> Listar</a></li>
+      <li><a href="javascript:void(0);" id="novo_contato"><i class="glyphicon glyphicon-plus"></i> Novo</a></li>
+    </ul>
+  </div>
 </nav>
-<div id="carregando" style="display: none; text-align: center;">
-<img src="./img/carregando.gif" />
+<div id="carregando" style="display: none; text-align: center;">
+  <img src="./img/carregando.gif" />
 </div>
