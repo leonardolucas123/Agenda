277ddba545ccb2f6e22a979a12ec5b88c0fd133c
leonardolucas123 <ll9607609@gmail.com>
2023-05-29 15:03:34 -0300

    a

--- cabecalho.php
+++ cabecalho.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>CRUD Contato - PHP Profissional</title>
-<link href="./css/bootstrap.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CRUD Contato - PHP Profissional</title>
+  <link href="./css/bootstrap.css" rel="stylesheet">
+</head>
 <body>
-<div class="container">
-<div class="jumbotron">
-<h1>CRUD - Contato - PHP Profissional</h1>
-</div>
+  <div class="container">
+    <div class="jumbotron">
+      <h1>CRUD - Contato - PHP Profissional</h1>
+    </div>
